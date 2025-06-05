998b566744a8b79f9273b9cdc78167bc49258ade
Vatt Sopheak <vattcarter@gmail.com>
2025-06-05 12:52:35 +0700

    clean up

--- terraform/aws-vpc-public-private/public-routes.tf
+++ terraform/aws-vpc-public-private/public-routes.tf
@@ -1,15 +1,4 @@
 resource "aws_route_table" "public" {
-  # vpc_id = aws_vpc.main.id
-
-  # route {
-  #   cidr_block = "0.0.0.0/0"
-  #   gateway_id = aws_internet_gateway.igw.id
-  # }
-
-  # tags = {
-  #   Name = "${local.env}-public"
-  # }
-
   vpc_id = aws_vpc.main.id
 
   route {
@@ -23,11 +12,6 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  # count = length(local.public_subnets)
-
-  # subnet_id      = aws_subnet.public[count.index].id
-  # route_table_id = aws_route_table.public.id
-
   for_each = local.public_subnets
 
   subnet_id      = aws_subnet.public[each.key].id
